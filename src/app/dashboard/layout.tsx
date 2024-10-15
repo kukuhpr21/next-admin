29e1688a5d698399189f8d891b12da6c7b10ea8b
Kukuh182 <kukuhpr21@gmail.com>
2024-10-15 20:28:50 +0700

    update layout dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,49 +1,56 @@
 "use client"
-import Logo from "@/components/Logo";
-import MenuList from "@/components/MenuList";
-import ToggleThemeButton from "@/components/ToggleThemeButton";
-import {Button, Layout, theme} from "antd";
-import { useState } from "react";
+import React, { useState } from 'react';
+import { Button, Layout, theme } from 'antd';
+import MenuList from '@/components/MenuList';
+import Logo from '@/components/Logo';
 import {MenuUnfoldOutlined, MenuFoldOutlined} from "@ant-design/icons";
 
-const { Header, Sider, Content} = Layout;
+const { Header, Content, Footer, Sider } = Layout;
+
+const siderStyle: React.CSSProperties = {
+  overflow: 'auto',
+  height: '100vh',
+  position: 'fixed',
+  insetInlineStart: 0,
+  top: 0,
+  bottom: 0,
+  scrollbarWidth: 'thin',
+  scrollbarColor: 'unset',
+};
 
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [darkTheme, setDarkTheme] = useState(true);
   const [collapsed, setCollapsed] = useState(false);
-  const toggleTheme = ()=> {
-    setDarkTheme(!darkTheme);
-  }
-
   const {
-    token : {colorBgContainer}
-  } = theme.useToken()
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  const paddingMainLayout = collapsed ? 'pl-[80px]' : 'pl-[200px]';
+
   return (
-    <>
-      <Layout className="h-screen overflow-auto">
-        <Sider 
-          collapsed={collapsed} 
-          collapsible
-          trigger={null}
-          theme={darkTheme ? 'dark' : 'light'} 
-          className="bg-white">
-          <Logo/>
-          <MenuList darkTheme={darkTheme}/>
-          <ToggleThemeButton darkTheme={darkTheme} toggleTheme={toggleTheme}/>
-        </Sider>
-        <Layout>
-          <Header className={`sticky p-0 bg-[${colorBgContainer}]`}>
-            <Button type="text" className="ml-4 text-white"  onClick={() => setCollapsed(!collapsed)} icon={collapsed ? <MenuUnfoldOutlined/> : <MenuFoldOutlined/>}/>
-          </Header>
-          <Layout>
-            <Content>{children}</Content>
-          </Layout>
-        </Layout>
+    <Layout hasSider >
+      <Sider 
+        style={siderStyle}
+        collapsed={collapsed} 
+        collapsible
+        trigger={null}>
+        <Logo/>
+        <MenuList darkTheme={true}/>
+      </Sider>
+      <Layout className={`bg-[${colorBgContainer}] ${paddingMainLayout}`}>
+        <Header className={`fixed w-full top-0 z-50 p-0 h-[66px] bg-[${colorBgContainer}]`}>
+        <Button type="text" className="ml-4 text-white"  onClick={() => setCollapsed(!collapsed)} icon={collapsed ? <MenuUnfoldOutlined/> : <MenuFoldOutlined/>}/>
+        </Header>
+        <Content className="mt-6 mr-4 mb-0 overflow-[initial] h-screen p-4">
+          {children}
+        </Content>
+        <Footer className="text-center">
+          Pobi Studio Design ©{new Date().getFullYear()} Created by Pobi Studio
+        </Footer>
       </Layout>
-    </>
-  );
+    </Layout>
+  )
 }
